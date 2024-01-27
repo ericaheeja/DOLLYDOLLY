fc060000b508186cfd8125ac97f80ccb0482e32d
ericaheeja <heejaerica@gmail.com>
2024-01-27 03:16:11 -0500

    Implement filter functionality on Dolls, Clothes, and Accessaries

--- src/pages/Accessaries.jsx
+++ src/pages/Accessaries.jsx
@@ -12,8 +12,42 @@ function Accessaries({ keyword }) {
   } = useQuery({ queryKey: ["products"], queryFn: async () => getProducts() });
 
   const [text, setText] = useState("");
+  const [container, setContainer] = useState(false);
+  const [check, setCheck] = useState({
+    brown: false,
+    pink: false,
+    red: false,
+    low: false,
+    medium: false,
+    high: false,
+  });
   const updateText = (text) => {
     setText(text);
+  };
+  const updateContainer = (container) => {
+    setContainer(container);
+  };
+  const updateCheck = (check) => {
+    setCheck(check);
+  };
+
+  const filterProducts = (product) => {
+    const { color, price } = product;
+    const { brown, pink, red, low, medium, high } = check;
+    if (text !== "" && !product.description.includes(text)) {
+      return false;
+    }
+    if ((brown && color !== "brown") || (pink && color !== "pink") || (red && color !== "red")) {
+      return false;
+    }
+    if (
+      (low && price >= 10000) ||
+      (medium && (price < 10000 || price >= 15000)) ||
+      (high && price < 15000)
+    ) {
+      return false;
+    }
+    return true;
   };
 
   const collectAccessaries = (product) => {
@@ -24,15 +58,21 @@ function Accessaries({ keyword }) {
 
   return (
     <>
-      <SearchHeader text={text} updateText={updateText} keyword={keyword} />
+      <SearchHeader
+        text={text}
+        updateText={updateText}
+        container={container}
+        updateContainer={updateContainer}
+        check={check}
+        updateCheck={updateCheck}
+        keyword={keyword}
+      />
       {isLoading && <p>Loading...</p>}
       {error && <p>{error}</p>}
       <ul className="grid grid-cols-1 md:grid-cols-3 lg-grid-cols-4 gap-4 p-4">
         {products &&
           products
-            .filter((product) => {
-              return text === "" ? product : product.description.includes(text);
-            })
+            .filter((product) => filterProducts(product))
             .map((product) => {
               return collectAccessaries(product);
             })}
